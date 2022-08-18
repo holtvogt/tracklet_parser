5261d71d0a8a5644daf70e2122bbe50bc7226b8a
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2022-08-18 16:23:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,42 @@
 from setuptools import find_packages, setup
 
 
-def get_readme():
-    with open('README.md', encoding='utf-8') as readme:
+def get_readme() -> str:
+    """Get README.md content.
+
+    Returns:
+        content (str): The README.md content
+    """
+
+    with open("README.md", encoding="utf-8") as readme:
         content = readme.read()
     return content
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(
-        name='tracklet_parser',
-        version='1.0.0',
-        description='A parser for tracklet labels created by the Computer'
-        'Vision Annotation Tool (CVAT).',
+        name="tracklet_parser",
+        version="1.0.0",
+        description=(
+            "A parser for tracklet labels in KITTI Raw Format 1.0 created by"
+            " the Computer Vision Annotation Tool (CVAT)."
+        ),
         long_description=get_readme(),
-        author='Bjoern Holtvogt',
-        author_email='bjoern.holtvogt@gmail.com',
-        keywords='tracklet, cvat, kitti',
-        url='https://github.com/holtvogt/tracklet_parser',
+        author="Bjoern Holtvogt",
+        author_email="bjoern.holtvogt@gmail.com",
+        keywords="tracklet, cvat, kitti, annotation",
+        url="https://github.com/holtvogt/tracklet_parser",
         packages=find_packages(),
-        license='MIT License',
-        setup_requires=['cython', 'numpy'],
+        license="MIT License",
+        python_requires=">=3.9",
         install_requires=[
-            'black',
-            'codespell',
-            'docformatter',
-            'isort',
-            'pandas',
-            'pre-commit',
+            "black",
+            "codespell",
+            "docformatter",
+            "isort",
+            "numpy",
+            "pandas",
+            "pre-commit",
+            "pylint",
         ],
     )
