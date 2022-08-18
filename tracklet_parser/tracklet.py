fe46af03c014c2f166981c4dd41cfa18bb3e1e40
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2022-08-18 18:50:52 +0200

    Add docstring

--- tracklet_parser/tracklet.py
+++ tracklet_parser/tracklet.py
@@ -28,70 +28,198 @@ class Tracklet:
         self._rotation_y = 0.0
 
     @property
-    def frame_number(self):
+    def frame_number(self) -> int:
+        """Get the frame number. This represents the index of the recording
+        within the dataset.
+
+        Returns:
+            int: The frame number
+        """
+
         return self._frame_number
 
     @property
-    def type(self):
+    def type(self) -> str:
+        """Get the object type. This can be 'Pedestrian', 'Cyclist',..
+
+        Returns:
+            str: The object type
+        """
+
         return self._type
 
     @property
-    def truncated(self):
+    def truncated(self) -> float:
+        """Get the truncation state. This is a range from 0 (non-truncated) to
+        1 (truncated) where truncated refers to the object leaving image
+        boundaries.
+
+        Returns:
+            float: The truncation state
+        """
+
         return self._truncated
 
     @property
-    def occluded(self):
+    def occluded(self) -> int:
+        """Get the occlusion state.
+
+        - 0: Fully visible
+        - 1: Partly occluded
+        - 2: Largely occluded
+        - 3: Unknown
+
+        Returns:
+            int: The occlusion state
+        """
+
         return self._occluded
 
     @property
-    def alpha(self):
+    def alpha(self) -> float:
+        """Get the observation angle `alpha` of the object ranging from.
+
+        [-PI, PI].
+
+        Returns:
+            float: The observation angle
+        """
+
         return 0.0
 
     @property
-    def bbox(self):
-        return {"left": 0.0, "top": 0.0, "right": 0.0, "bottom": 0.0}
+    def bbox(self) -> Dict[str, float]:
+        """Get the 2D bounding box of the object in the image and contains
+        left.
+
+        , top, right, bottom pixel coordinates.
+
+        Returns:
+            Dict[str, float]: The 2D bounding box
+        """
+
+        return deepcopy(self._bbox)
 
     @property
-    def dimensions(self):
+    def dimensions(self) -> Dict[str, float]:
+        """The 3D object dimensions height, width and length (in meters).
+
+        Returns:
+            Dict[str, float]: The 3D object dimensions
+        """
+
         return deepcopy(self._dimensions)
 
     @property
-    def location(self):
+    def location(self) -> Dict[str, float]:
+        """The 3D object location x, y and z in LiDAR coordinates (in meters).
+
+        Returns:
+            Dict[str, float]: The 3D object location
+        """
+
         return deepcopy(self._location)
 
     @property
-    def rotation_y(self):
+    def rotation_y(self) -> float:
+        """The rotation around Y-axis in camera coordinates ranging from.
+
+        [-PI, PI].
+
+        Note: Keep in mind that this is in camera coordinates!
+
+        Returns:
+            float: The rotation angle around Y-axis
+        """
+
         return self._rotation_y
 
     @frame_number.setter
     def frame_number(self, frame_number: int):
+        """Set the frame number.
+
+        Arguments:
+            frame_number (int): The frame number
+        """
+
         self._frame_number = frame_number
 
     @type.setter
     def type(self, object_type: str):
+        """Set the object type.
+
+        Arguments:
+            object_type (str): The object type
+        """
+
         self._type = object_type
 
     @truncated.setter
     def truncated(self, truncated: float):
+        """Set the truncation state.
+
+        Arguments:
+            truncated (float): The truncation state
+        """
+
         self._truncated = truncated
 
     @occluded.setter
     def occluded(self, occluded: int):
+        """Set the occlusion state.
+
+        Arguments:
+            occluded (int): The occlusion state
+        """
+
         self._occluded = occluded
 
     @alpha.setter
     def alpha(self, alpha: float):
+        """Set the observation angle of the object.
+
+        Arguments:
+            alpha (float): The observation angle of the object
+        """
+
         self._alpha = alpha
 
     def put_bbox(self, key: str, value: float):
+        """Set a new pixel coordinate for the 2D bounding box.
+
+        Arguments:
+            key (str): The 2D bounding orientation (left, top, right, bottom)
+            value (str): The pixel coordinate
+        """
+
         self._bbox[key] = value
 
     def put_dimension(self, key: str, value: float):
+        """Set a new dimension value for the 3D object.
+
+        Arguments:
+            key (str): The dimension direction (height, width, length)
+            value (str): The dimension value
+        """
+
         self._dimensions[key] = value
 
     def put_location(self, key: str, value: float):
+        """Set a new coordinate for the 3D object.
+
+        Arguments:
+            key (str): The axis (x, y, z)
+            value (str): The coordinate
+        """
+
         self._location[key] = value
 
     @rotation_y.setter
     def rotation_y(self, rotation_y: float):
+        """Set the rotation angle.
+
+        Arguments:
+            rotation_y (float). The rotation angle
+        """
+
         self._rotation_y = rotation_y
